5c398b1406753c1ea19bf36c0e7426310b86b259
Ismazgz14 <71751871+Ismazgz14@users.noreply.github.com>
2020-09-26 12:19:47 +0200

    Add files via upload

---
+++ tabla.py
@@ -0,0 +1,8 @@
+def tabla():
+    n_tabla=input("dime la tabla: ")
+    print "tabla del ", n_tabla
+    for cont in range(1,10):
+     print n_tabla," x ",cont , " = ", n_tabla*cont
+tabla()
+    
+   
