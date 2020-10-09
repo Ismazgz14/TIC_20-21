dbe5b95f41ec2d23a0419517e1874b3ca3a201b7
Ismazgz14 <71751871+Ismazgz14@users.noreply.github.com>
2020-10-09 15:39:55 +0200

    Add files via upload

---
+++ contrasena_3.py
@@ -0,0 +1,7 @@
+'''Escribe un programa que genere'''
+def contrasena():
+    nombre=raw_input("Introduce el nombre: ")
+    apellido=raw_input("Introduce el apellido: ")
+    print nombre," ",apellido, "eres el impostor?"
+    print "Tu lindo nombre empieza por la ",3*nombre+2*apellido
+contrasena()
