a03d055ac8be83c4cc8878b4124bdbc7e381352a
Ismazgz14 <71751871+Ismazgz14@users.noreply.github.com>
2020-11-05 12:39:00 +0100

    Add files via upload

---
+++ ejercicio 10 .py
@@ -0,0 +1,10 @@
+'''Realizar un programa que al recibir un número entero
+muestre por pantalla los 3 números anteriores y
+los 3 números siguientes al número recibido.'''
+def ejercicio_10():
+    numero=input ("Introduce un numero entero:")
+    print (numero-1),(numero-2),(numero-3)
+    print (numero+1),(numero+2),(numero+3)
+    
+ejercicio_10()
+    
